dc3aacd5508381e10fd391a9094b7569eed632f4
Walter Junior <wpmj1277@gmail.com>
2021-03-14 18:13:35 -0300

    Create Transaction Notifier

--- app/Application/Services/Transaction/NotifyTransaction/TransactionNotifier.php
+++ app/Application/Services/Transaction/NotifyTransaction/TransactionNotifier.php
@@ -2,7 +2,14 @@
 
 namespace App\Application\Services\Transaction\NotifyTransaction;
 
+/**
+ * Interface TransactionNotifier
+ * @package App\Application\Services\Transaction\NotifyTransaction
+ */
 interface TransactionNotifier
 {
-
+    /**
+     * @return bool
+     */
+    public function sendNotification(): bool;
 }
